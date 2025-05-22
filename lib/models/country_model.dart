5702d9fbd15d98aab3fbc864c241ea71da82cd46
Jobaieer <jobaieer.cse@proton.me>
2025-05-22 21:12:03 +0600

    feat(models): add Country model for local country data

---
+++ lib/models/country_model.dart
@@ -0,0 +1,16 @@
+// Here created country model where arranged by data type. -> country er model jekan e data r goton setup kora hoica
+class Country {
+  final String flag;
+  final String name;
+  final String capital;
+  final String population;
+  final String region;
+
+  Country({
+    required this.flag,
+    required this.name,
+    required this.capital,
+    required this.population,
+    required this.region,
+  });
+}

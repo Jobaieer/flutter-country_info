730312817698fbd077baea7966e5ddea032fb71e
Jobaieer <jobaieer.cse@proton.me>
2025-05-22 21:50:41 +0600

    feat(data): added countries data

---
+++ lib/data/country_data.dart
@@ -0,0 +1,98 @@
+// local babe raka country list
+import '../models/country_model.dart';
+
+List<Country> countries = [
+  Country(
+    flag: 'https://flagcdn.com/w320/bd.png',
+    name: 'Bangladesh',
+    capital: 'Dhaka',
+    population: '165M',
+    region: 'Asia',
+  ),
+  Country(
+    name: 'Germany',
+    capital: 'Berlin',
+    region: 'Europe',
+    population: '83M',
+    flag: 'https://flagcdn.com/w320/de.png',
+  ),
+  Country(
+    name: 'France',
+    capital: 'Paris',
+    region: 'Europe',
+    population: '67M',
+    flag: 'https://flagcdn.com/w320/fr.png',
+  ),
+  Country(
+    name: 'Japan',
+    capital: 'Tokyo',
+    region: 'Asia',
+    population: '125M',
+    flag: 'https://flagcdn.com/w320/jp.png',
+  ),
+  Country(
+    name: 'Brazil',
+    capital: 'Brasília',
+    region: 'South America',
+    population: '215M',
+    flag: 'https://flagcdn.com/w320/br.png',
+  ),
+  Country(
+    name: 'Canada',
+    capital: 'Ottawa',
+    region: 'North America',
+    population: '39M',
+    flag: 'https://flagcdn.com/w320/ca.png',
+  ),
+  Country(
+    name: 'Australia',
+    capital: 'Canberra',
+    region: 'Oceania',
+    population: '26M',
+    flag: 'https://flagcdn.com/w320/au.png',
+  ),
+  Country(
+    name: 'United States',
+    capital: 'Washington, D.C.',
+    region: 'North America',
+    population: '331M',
+    flag: 'https://flagcdn.com/w320/us.png',
+  ),
+  Country(
+    name: 'United Kingdom',
+    capital: 'London',
+    region: 'Europe',
+    population: '67M',
+    flag: 'https://flagcdn.com/w320/gb.png',
+  ),
+  Country(
+    name: 'Saudi Arabia',
+    capital: 'Riyadh',
+    region: 'Asia',
+    population: '36M',
+    flag: 'https://flagcdn.com/w320/sa.png',
+  ),
+  Country(
+    name: 'Qatar',
+    capital: 'Doha',
+    region: 'Asia',
+    population:' 28M',
+    flag: 'https://flagcdn.com/w320/qa.png',
+  ),
+  Country(
+    name: 'Malaysia',
+    capital: 'Kuala Lumpur',
+    region: 'Asia',
+    population: '33M',
+    flag: 'https://flagcdn.com/w320/my.png',
+  ),
+  Country(
+    name: 'South Korea',
+    capital: 'Seoul',
+    region: 'Asia',
+    population: '52M',
+    flag: 'https://flagcdn.com/w320/kr.png',
+  ),
+
+
+];
